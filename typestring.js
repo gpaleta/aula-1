c3e835db5253e0fc1e2672fe36b3d29d671fc411
gpaleta <128159891+gpaleta@users.noreply.github.com>
2024-02-22 09:45:30 -0300

    Add files via upload

---
+++ typestring.js
@@ -0,0 +1,10 @@
+const texto1 = "olá, mundo!!!";
+const texto2 = 'olá, mundo!!!';
+const senha = 'senhasupersegura!';
+const stringdenumero = '4893';
+
+const citacao = 'meu nome é ';
+const nome = "L"
+
+console.log(citacao + nome)
+// concatenação (+)
